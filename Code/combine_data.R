38a75d136feb35b6d16724f543fdce01d01ace59
iuanx <juangantwelve@outlook.com>
2025-12-05 16:09:54 -0700

    More tweaks
    got rid of most recent custody date so we dont predict with a date

--- Code/combine_data.R
+++ Code/combine_data.R
@@ -1,3 +1,5 @@
+library(lubridate)
+
 marks <- read.csv("CSV Files/marks_unique_counts.csv")
 people <- read.csv("CSV Files/person_cleaned.csv")
 sentencing <- read.csv("CSV Files/sentencing_cleaned.csv")
@@ -6,6 +8,7 @@ merged_data <- merge(marks, people, on = "id")
 merged_data <- merge(merged_data, sentencing, on = "id")
 
 merged_data_end <- merged_data %>% mutate(appx_age = year_adm - year_born) %>% 
-  select(-offender_status, -year_born)
+  select(-offender_status, -year_born, -most_recent_custody_date)
+
 
 write.csv(merged_data_end, "CSV Files/merged_data.csv", row.names = F)
